5c007aa5ae50efbcdb1066c4d820eacbbac50db1
이호진 <gommind@naver.com>
2023-10-23 12:01:51 +0900

    test github

---
+++ src/test/java/com/playdata/gittest/GithubTest2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTest2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
